12b6b08ba02dca771cd942f455c0e6a59a070aba
Tynka <marcia03835@gmail.com>
2022-07-20 15:00:01 +0200

    Written sub_strings method

---
+++ SumStrungs/sub_strings.rb
@@ -0,0 +1,17 @@
+def sub_strings(dictionary, string)
+    string.downcase!
+    substrings = Hash.new(0)
+
+    dictionary.each do |word|
+        finds_array = string.scan(word)
+        finds_number = finds_array.length
+        substrings[word] = finds_number if finds_number != 0
+    end
+
+    puts substrings
+    substrings
+end 
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+sub_strings(dictionary, "Howdy partner, sit down! How's it going?")
